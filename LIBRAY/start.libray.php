ecbda22ec75fb934e7bee85a384dc6b2a444d809
groszek20 <pea@op.pl>
2017-11-11 21:30:13 +0100

    Module loader

--- LIBRAY/start.libray.php
+++ LIBRAY/start.libray.php
@@ -4,6 +4,11 @@ $meta = new MetaTags($_GET['page'], FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FA
                      FALSE, FALSE, FALSE, FALSE, FALSE, "screen", "icon", 
                      "jQuery,script,jquery.localscroll-1.2.5,coda-slider,jquery.scrollTo-1.3.3,jquery.serialScroll-1.2.1");
 
-
-
+ModuleLoader::load("LoginForm");
+ModuleLoader::load("Menu");
+ModuleLoader::load("Slogan");
+ModuleLoader::load("BoxWstep");
+ModuleLoader::load("BoxFirma");
+ModuleLoader::load("BoxRight");
+ModuleLoader::load("Footer");
 ?>
